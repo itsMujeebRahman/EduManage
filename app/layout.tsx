ad12ec3acf12006898208ff7bb394f77a12ee0d4
mujeeb <mujeebrahman8943@gmail.com>
2025-07-12 17:36:57 +0530

    changed the default state of NavSelect

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [selectMenu, setSelectMenu] = useState<string>("Teachers");
+  const [selectMenu, setSelectMenu] = useState<string>("/");
   const [enableNav, setEnableNav] = useState<boolean>(false);
 
   return (
